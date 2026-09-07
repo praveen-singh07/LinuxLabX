63fd3106d4f1c7a34b8dfd7f8a46e2a87f8c114c
Praveen Singh <praveensingh3967@gmail.com>
2026-09-07 17:19:19 +0530

    feat: configure database with SQLAlchemy

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from app.core.database import engine, Base
+
 
 app = FastAPI(
     title="LinuxLabX",
     description="Gamified Linux Learning Platform",
     version="1.0.0"
 )
+
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
